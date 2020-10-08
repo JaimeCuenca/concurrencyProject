b8e5cd6caea46cabb088e1111a480ec0957e2679
JaimeCuenca <jaime.cuenca.caro@gmail.com>
2020-10-08 13:05:20 +0200

    trycatch

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        ThreadWithDrawCash t1 = new ThreadWithDrawCash();
-        t1.start();
-        t1.join();
-        ThreadWithDrawCash t2 = new ThreadWithDrawCash();
-        t2.start();
-        t2.join();
-        ThreadWithDrawCash t3 = new ThreadWithDrawCash();
-        t3.start();
-        t3.join();
+    public static void main(String[] args) {
+        try {
+            ThreadWithDrawCash t1 = new ThreadWithDrawCash();
+            t1.start();
+            t1.join();
+            ThreadWithDrawCash t2 = new ThreadWithDrawCash();
+            t2.start();
+            t2.join();
+            ThreadWithDrawCash t3 = new ThreadWithDrawCash();
+            t3.start();
+            t3.join();
+        }catch (InterruptedException e){
+            e.getMessage();
+        }
     }
 }
